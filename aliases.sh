7bc530fdb7fe9d71f4c5d447619b5004ec437b8a
davealone69-gif <davealone69@gmail.com>
2026-08-08 15:40:12 +0800

    Add standalone aliases file

---
+++ aliases.sh
@@ -0,0 +1,18 @@
+# Termux Full Setup - Useful aliases
+# Source this file: source aliases.sh
+# Or it is automatically added by setup.sh
+
+alias ll='ls -la --color=auto'
+alias la='ls -A'
+alias l='ls -CF'
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+alias gl='git log --oneline --graph --decorate'
+alias gd='git diff'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias omni='omniroute'
+alias update='pkg update && pkg upgrade -y'
+alias cls='clear'
